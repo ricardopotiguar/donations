53c99f35c0684dd849c78df776f6367838944493
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-11-26 16:07:13 -0300

    Ajuste caminho das rotas de usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,14 +5,17 @@ import { getAll, create, update, deleteUser } from '../controllers/userControlle
 import { userValidation } from '../middlewares/validationMiddleware.js'
 import { createUserNeedsController, getAllUserNeedsController, updateUserNeedsController, deleteUserNeedsController } from '../controllers/userNeedsController.js'
 
-router.get('/usuarios', getAll)
-router.post('/usuarios', userValidation, create) 
-router.put('/usuarios/:id', update)
-router.delete('/usuarios/:id', deleteUser)
+router.get('/api/v1/usuarios', getAll)
+router.post('/api/v1/usuarios', userValidation, create) 
+router.put('/api/v1/usuarios/:id', update)
+router.delete('/api/v1/usuarios/:id', deleteUser)
 
 router.post('/api/v1/necessidades', createUserNeedsController) 
 router.get('/api/v1/necessidades', getAllUserNeedsController) 
 router.put('/api/v1/necessidades/:id', updateUserNeedsController) 
 router.delete('/api/v1/necessidades/:id', deleteUserNeedsController)
+
+
+
 
 export default router
