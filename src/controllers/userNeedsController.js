1235fda7d7f8d95295e73646ab243ab82d1b41b7
Ricardo Potiguar <ricardopotiguar@gmail.com>
2024-12-18 09:44:29 -0300

    Alterado imagens de donations para salvar no cloudinaty

--- src/controllers/userNeedsController.js
+++ src/controllers/userNeedsController.js
@@ -1,8 +1,11 @@
 import { createUserNeedsService, getAllUserNeedsService, updateUserNeedsService, deleteUserNeedsService } from '../services/userNeedsService.js'
 
+import { v2 as cloudinary } from 'cloudinary'
 
-async function getAllUserNeedsController(request, response){
-    try{
+import path from 'path'
+
+async function getAllUserNeedsController(request, response) {
+    try {
         let usersNeeds = []
         usersNeeds = await getAllUserNeedsService(request)
         return response.status(200).json(usersNeeds)
@@ -11,30 +14,53 @@ async function getAllUserNeedsController(request, response){
     }
 }
 
-async function createUserNeedsController(request, response){
+async function createUserNeedsController(request, response) {
     try {
         const { title, description, userId, quantity, type } = request.body;
         const imagePath = request.file ? `${process.env.BASE_URL}/uploads/userNeedsImages/${request.file.filename}` : null;
-    
+
+        console.log(request.file)
+
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+
+        // Caminho absoluto do arquivo
+        const filePath = path.resolve(request.file.path);
+
+        // Upload an image
+        const uploadResult = await cloudinary.uploader
+            .upload(
+                filePath, {
+                public_id: request.file.filename,
+            }
+            )
+            .catch((error) => {
+                console.log(error);
+            });
+
+
         // Envia os dados ao serviço
         await createUserNeedsService({
-          userId: Number(userId),
-          title,
-          description,
-          quantity: Number(quantity),
-          type,
-          imageUrl: imagePath, // Adiciona o caminho da imagem
+            userId: Number(userId),
+            title,
+            description,
+            quantity: Number(quantity),
+            type,
+            imageUrl: uploadResult.secure_url, // Adiciona o caminho da imagem
         });
 
 
-/*         await createUserNeedsService(request.body) */
+        /*         await createUserNeedsService(request.body) */
         return response.status(201).send(request.body)
     } catch (error) {
         return response.status(500).json({ message: 'An error occurred while creating the userNeeds.', datail: error.message });
     }
 }
 
-async function updateUserNeedsController(request, response){
+async function updateUserNeedsController(request, response) {
     try {
         await updateUserNeedsService(request)
         return response.status(200).send(request.body)
@@ -44,10 +70,10 @@ async function updateUserNeedsController(request, response){
 
 }
 
-async function deleteUserNeedsController(request, response){
+async function deleteUserNeedsController(request, response) {
     try {
-        await deleteUserNeedsService (request.params)
-        return response.status(200).send({message:"Necessidades do usuário excluídas com sucesso!"})
+        await deleteUserNeedsService(request.params)
+        return response.status(200).send({ message: "Necessidades do usuário excluídas com sucesso!" })
     } catch (error) {
         return response.status(500).json({ message: 'An error occurred while deleting the user needs.', datail: error.message });
     }
